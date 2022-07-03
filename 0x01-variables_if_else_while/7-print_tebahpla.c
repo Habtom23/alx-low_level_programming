4c46f31c872733f85928ca4e071291ce51e8102b
Habtom23 <habtomst221@gmail.com>
2022-07-03 01:50:27 -0700

    Fixed Task-7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * Return: ALways 0 (success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num < 10; num++)
-		putchar(num + '0');
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
