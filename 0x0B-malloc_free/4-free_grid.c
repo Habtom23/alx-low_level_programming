3de640b02cb43ac3d3d9c4db6da96ab543a0d54e
Habtom23 <habtomst221@gmail.com>
2023-10-04 00:43:59 +0300

    hot fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,15 +4,30 @@
   * free_grid - frees a 2D grid
   * @grid: pointer to pointer.
   * @height: height of grid.
-  * Return: void.
+  * Return: 0 if success
   */
 void free_grid(int **grid, int height)
 {
-	int i;
+	if (grid == NULL)
+	{
+		return;
+	}
+
 
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
 	free(grid);
+}
+
+int main(void)
+{
+
+	int height = 3;
+	int **grid = alloc_grid(height);
+
+	free_grid(grid, height);
+
+	return (0);
 }
